cfca45147605c15275e0acdb18cbb0510ebb31e8
David Fontenot <fontenod@onid.oregonstate.edu>
2014-02-12 22:07:15 -0800

    fixed indentation, added selfs to function calls

--- IBTLTrans/Stages/Parser.py
+++ IBTLTrans/Stages/Parser.py
@@ -1,16 +1,20 @@
-class Parser():
-
+class Parser:
     def __init__(self, tokens):
         self._tokens = tokens
-        
+    
     # this is the T production in revisedgrammar.txt
-     def parse(self):
-        (tokens, error) = s(self._tokens)
+    def parse(self):
+        (tokens, error) = self.s(self._tokens)
+
+        if not error:
+            print("parser succeeded")
+        else:
+            print("parser failed: %s" % tokens)
         
         return not error
         
     def s(self, tokens):
-        (exprToks, error) = expr(tokens)
+        (exprToks, error) = self.expr(tokens)
         if error:
             # nothing else to do for this production, return up an error
             return (tokens, True)
@@ -22,20 +26,20 @@ class Parser():
                 return ([], False)
             else:
                 # continue consuming tokens, if possible
-                return s(exprToks)
+                return self.s(exprToks)
             
     def expr(self, tokens):
-        (numexprToks, error) = numexpr(tokens)
+        (numexprToks, error) = self.numexpr(tokens)
         
         if error:
             # must not be an integer expression
-            (strexprToks, error) = strexpr(tokens)
+            (strexprToks, error) = self.strexpr(tokens)
 
             if error:
-                (boolexprToks, error) = boolexpr(tokens)
+                (boolexprToks, error) = self.boolexpr(tokens)
 
                 if error:
-                    (stmtToks, error) = stmt(tokens)
+                    (stmtToks, error) = self.stmt(tokens)
 
                     if error:
                         # return up an error
@@ -70,16 +74,16 @@ class Parser():
 
             toks = tokens[1:]
 
-            if numbinopPred(toks[0]) and numunopPred(toks[0]):
+            if self.numbinopPred(toks[0]) and self.numunopPred(toks[0]):
                 # must be one of the operators both have in common, see how many exprs can be attained
 
-                (numexpr1Toks, error) = numexpr(toks[1:])
+                (numexpr1Toks, error) = self.numexpr(toks[1:])
                 if error:
                     # not a valid numexpr
                     return (tokens, True)
                 else:
                     # try getting another expression out of it
-                    (numexpr2Toks, error) = numexpr(numexpr1Toks)
+                    (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
                     if error:
                         # must be a unop
                         
@@ -97,23 +101,31 @@ class Parser():
                             # statement not properly closed
                             return (tokens, True)
             else:
-                if numbinop(toks[0]):
-                else if numunop(toks[0]):
-                    (numexpr1Toks, error) = numexpr(toks[1:])
+                if self.numbinopPred(toks[0]):
+                    (numexpr1Toks, error) = self.numexpr(toks[1:])
+                    if error:
+                        return (tokens, True)
+
+                    (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
+                    if error:
+                        return (tokens, True)
+                    if numexpr2Toks[0] == "T_RBRACKET":
+                        return (numexpr2Toks[1:], False)
+                    else:
+                        return (tokens, True)
+                elif self.numunopPred(toks[0]):
+                    (numexpr1Toks, error) = self.numexpr(toks[1:])
                     
                     if error:
                         return (tokens, True)
                     else:
                         # check for right bracket
-                        if numexpr1Toks[1:] == "T_RBRACKET":
+                        if numexpr1Toks[0] == "T_RBRACKET":
                             return (numexpr1Tok[1:], False)
                         else:
                             return (tokens, True)
                 else:
                     return (tokens, False)
-                    
-                # return up an error
-                return (tokens, True)
     
     # 
     # PREDICATES
