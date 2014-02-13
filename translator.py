eb767d9c7f901c284628f86d000d843a3899f8ce
David Fontenot <fontenod@onid.oregonstate.edu>
2014-02-12 22:07:34 -0800

    added the parser to the translator script

--- translator.py
+++ translator.py
@@ -3,18 +3,24 @@
 import sys
 
 from IBTLTrans.Stages.Tokenizer import Tokenizer
+from IBTLTrans.Stages.Parser import Parser
 from IBTLTrans.Utils.Utils import Utils
         
 def main():    
     if len(sys.argv) != 2:
-        print("Usage tokenizer.py <filename>")
+        print("Usage translator.py <filename>")
     else:
         f = open(sys.argv[1], "r")
         t = Tokenizer(f.read())
         toks = t.tokenize()
         
+        # tokenize
         for tok in toks:
             print(tok)
+
+        # parser
+        p = Parser(toks)
+        p.parse()
 
         f.close()
         
