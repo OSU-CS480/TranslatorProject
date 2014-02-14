824648151d3bf9e30b6043f227db8a1598c8cfbe
David Fontenot <fontenod@onid.oregonstate.edu>
2014-02-13 16:57:09 -0800

    can parse multiple separate expressions now

--- IBTLTrans/Stages/Parser.py
+++ IBTLTrans/Stages/Parser.py
@@ -44,172 +44,213 @@ class Parser:
                     newGraph["s"] = exprs
                     return (sToks, error, newGraph)
             
-    def expr(self, tokens, graph):
+    def expr(self, tokens, graph, prime=False):
+        newGraph = {}
+        exprs = []
+
+        # expr' specific productions
+        if prime:
+            if self.constPred(tokens[0]):
+                newGraph[tokens[0]] = "some const"
+                return (tokens[1:], False, newGraph)
+            elif tokens[0] == "T_ID":
+                newGraph["T_ID"] = "some id"
+                return (tokens[1:], False, newGraph)
+
+        # if expr' (prime set to True) hasn't returned by here, must be some sort of expression
         if tokens[0] != "T_LBRACKET":
             return (tokens, True, graph)
 
         # could be an assignment
         if tokens[1] == "T_ASSIGN":
-            (exprToks, error, exprGraph) = self.exprPrime(tokens[2:], graph)
+            # in expr' assignment is not allowed
+            if prime:
+                return (tokens, True, graph)
+
+            (exprToks, error, exprGraph) = self.expr(tokens[2:], graph, True)
 
             if error:
                 return (tokens, True, graph)
             else:
-                # todo
+                # an identifier must follow
+                if exprToks[0] != "T_ID" or exprToks[1] != "T_RBRACKET":
+                    return (tokens, True, graph)
+                else:
+                    newGraph["assign"] = [exprGraph, "id (assign)"]
+                    return (exprToks[1:-1], error, newGraph)
         else:
             # must be binop or unop
 
             # T_MINUS can be either, one or more expressions may follow
             if tokens[1] == "T_MINUS":
-                (expr1Toks, error, expr1Graph) = self.exprPrime(tokens[2:], graph)
+                (expr1Toks, error, expr1Graph) = self.expr(tokens[2:], graph, True)
 
                 if error:
                     # error on first expression, not the right production
                     return (tokens, True, graph)
                 
-                # todo: add the last expressions graph here
-                (expr2Toks, error, expr2Graph) = self.exprPrime(expr2Toks, graph)
+                exprs.append(expr1Graph)
+                (expr2Toks, error, expr2Graph) = self.expr(expr2Toks, graph, True)
                 
                 if error:
                     # could have meant the unop version of -
-                    return (expr1Toks, False, newGraph)
+
+                    if expr1Toks[0] == "T_RBRACKET":
+                        newGraph["T_MINUS"] = exprs
+                        return (expr1Toks[1:], False, newGraph)
+                    else:
+                        return (tokens, True, graph)
                 else:
                     # binop verion of -
-                    # todo: add both expressionss graphs here
-                    return (expr2Toks, False, newGraph)
+                    if expr2Toks[0] == "T_RBRACKET":
+                        exprs.append(expr2Graph)
+                        newGraph["T_MINUS"] = exprs
+                        return (expr2Toks[1:], False, newGraph)
+                    else:
+                        return (tokens, True, graph)
             else:
-                if binopPred(tokens[2]):
+                if self.binopPred(tokens[1]):
                     # strictly binary operation
 
-                    (expr1Toks, error, expr1Graph) = self.exprPrime(tokens[2:], graph)
+                    (expr1Toks, error, expr1Graph) = self.expr(tokens[2:], graph, True)
 
                     if error:
                         return (tokens, True, graph)
                 
-                    # todo: add the last expressions graph here
-                    (expr2Toks, error, expr2Graph) = self.exprPrime(expr2Toks, graph)
+                    exprs.append(expr1Graph)
+                    (expr2Toks, error, expr2Graph) = self.expr(expr1Toks, graph, True)
                 
                     if error:
                         return (tokens, True, graph)
                     else:
-                        return (expr2Toks, error, newGraph)
+                        if expr2Toks[0] == "T_RBRACKET":
+                            exprs.append(expr2Graph)
+                            newGraph[tokens[1]] = exprs
+                            return (expr2Toks[1:], error, newGraph)
+                        else:
+                            return (tokens, True, graph)
                 else:
                     # strictly unary operation
 
-                    (expr1Toks, error, expr1Graph) = self.exprPrime(tokens[2:], graph)
+                    (exprToks, error, exprGraph) = self.expr(tokens[2:], graph, True)
 
                     if error:
                         # error on first expression, not the right production
                         return (tokens, True, graph)
                     else:
-                        return (expr1Toks, error, newGraph)
+                        if exprToks[0] == "T_RBRACKET":
+                            exprs.append(exprGraph)
+                            newGraph[tokens[1]] = exprs
+                            return (exprToks[1:], error, newGraph)
+                        else:
+                            return (tokens, True, graph)
 
-    def exprPrime(self, tokens, graph):
-        #
+    # def numexpr(self, tokens):
+    #     if tokens[0] != "T_LBRACKET":
+    #         # must be either an integer or an identifier
 
-    def numexpr(self, tokens):
-        if tokens[0] != "T_LBRACKET":
-            # must be either an integer or an identifier
+    #         if tokens[0] != "T_INT" and tokens[0] != "T_FLOAT":
+    #             # has to be an identifier
 
-            if tokens[0] != "T_INT" and tokens[0] != "T_FLOAT":
-                # has to be an identifier
+    #             if tokens[0] != "T_ID":
+    #                 # return up an error
+    #                 return (tokens, True)
+    #             else:
+    #                 return (tokens[1:], False)
+    #         else:
+    #             # both cases (int or float) will remove one terminal
+    #             return (tokens[1:], False)
+    #     else:
+    #         # either a unary operation or binary operation
 
-                if tokens[0] != "T_ID":
-                    # return up an error
-                    return (tokens, True)
-                else:
-                    return (tokens[1:], False)
-            else:
-                # both cases (int or float) will remove one terminal
-                return (tokens[1:], False)
-        else:
-            # either a unary operation or binary operation
+    #         toks = tokens[1:]
 
-            toks = tokens[1:]
+    #         if self.numbinopPred(toks[0]) and self.numunopPred(toks[0]):
+    #             # must be one of the operators both have in common, see how many exprs can be attained
 
-            if self.numbinopPred(toks[0]) and self.numunopPred(toks[0]):
-                # must be one of the operators both have in common, see how many exprs can be attained
-
-                (numexpr1Toks, error) = self.numexpr(toks[1:])
-                if error:
-                    # not a valid numexpr
-                    return (tokens, True)
-                else:
-                    # try getting another expression out of it
-                    (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
-                    if error:
-                        # must be a unop
+    #             (numexpr1Toks, error) = self.numexpr(toks[1:])
+    #             if error:
+    #                 # not a valid numexpr
+    #                 return (tokens, True)
+    #             else:
+    #                 # try getting another expression out of it
+    #                 (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
+    #                 if error:
+    #                     # must be a unop
                         
-                        if numexpr1Toks[0] == "T_RBRACKET":
-                            return (numexpr1Toks[1:], False)
-                        else:
-                            # statement not properly closed
-                            return (tokens, True)
-                    else:
-                        # must be a binop
+    #                     if numexpr1Toks[0] == "T_RBRACKET":
+    #                         return (numexpr1Toks[1:], False)
+    #                     else:
+    #                         # statement not properly closed
+    #                         return (tokens, True)
+    #                 else:
+    #                     # must be a binop
 
-                        if numexpr2Toks[0] == "T_RBRACKET":
-                            return (numexpr2Toks[1:], False)
-                        else:
-                            # statement not properly closed
-                            return (tokens, True)
-            else:
-                if self.numbinopPred(toks[0]):
-                    (numexpr1Toks, error) = self.numexpr(toks[1:])
-                    if error:
-                        return (tokens, True)
+    #                     if numexpr2Toks[0] == "T_RBRACKET":
+    #                         return (numexpr2Toks[1:], False)
+    #                     else:
+    #                         # statement not properly closed
+    #                         return (tokens, True)
+    #         else:
+    #             if self.numbinopPred(toks[0]):
+    #                 (numexpr1Toks, error) = self.numexpr(toks[1:])
+    #                 if error:
+    #                     return (tokens, True)
 
-                    (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
-                    if error:
-                        return (tokens, True)
-                    if numexpr2Toks[0] == "T_RBRACKET":
-                        return (numexpr2Toks[1:], False)
-                    else:
-                        return (tokens, True)
-                elif self.numunopPred(toks[0]):
-                    (numexpr1Toks, error) = self.numexpr(toks[1:])
+    #                 (numexpr2Toks, error) = self.numexpr(numexpr1Toks)
+    #                 if error:
+    #                     return (tokens, True)
+    #                 if numexpr2Toks[0] == "T_RBRACKET":
+    #                     return (numexpr2Toks[1:], False)
+    #                 else:
+    #                     return (tokens, True)
+    #             elif self.numunopPred(toks[0]):
+    #                 (numexpr1Toks, error) = self.numexpr(toks[1:])
                     
-                    if error:
-                        return (tokens, True)
-                    else:
-                        # check for right bracket
-                        if numexpr1Toks[0] == "T_RBRACKET":
-                            return (numexpr1Tok[1:], False)
-                        else:
-                            return (tokens, True)
-                else:
-                    return (tokens, False)
+    #                 if error:
+    #                     return (tokens, True)
+    #                 else:
+    #                     # check for right bracket
+    #                     if numexpr1Toks[0] == "T_RBRACKET":
+    #                         return (numexpr1Tok[1:], False)
+    #                     else:
+    #                         return (tokens, True)
+    #             else:
+    #                 return (tokens, False)
     
-    def strexpr(self, tokens, graph):
-        if tokens[0] == "T_LBRACKET":
-            if tokens[1] != "T_PLUS":
-                return (tokens, True, graph)
+    # def strexpr(self, tokens, graph):
+    #     if tokens[0] == "T_LBRACKET":
+    #         if tokens[1] != "T_PLUS":
+    #             return (tokens, True, graph)
 
-            (strexpr1Toks, error, g1) = self.strexpr(tokens[2:], graph)
-            if error:
-                return (tokens, True, graph)
+    #         (strexpr1Toks, error, g1) = self.strexpr(tokens[2:], graph)
+    #         if error:
+    #             return (tokens, True, graph)
 
-            (strexpr2Toks, error, g2) = self.strexpr(strexpr1Toks, graph)
-            if error:
-                return (tokens, True, graph)
+    #         (strexpr2Toks, error, g2) = self.strexpr(strexpr1Toks, graph)
+    #         if error:
+    #             return (tokens, True, graph)
             
-            if strexpr2Toks[0] == "T_RBRACKET":
-                graph["T_PLUS"]=[g1,g2]
-                return (strexpr2Toks[1:], False, graph)
-            else:
-                return (tokens, True)
-        elif tokens[0] == "T_CONSTSTR":
-            return (tokens[1:], False, {"T_CONSTSTR": "a str"})
-        elif tokens[0] == "T_ID":
-            return (tokens[1:], False, {"T_ID": "an id"})
-        else:
-            return (tokens, True, graph)
+    #         if strexpr2Toks[0] == "T_RBRACKET":
+    #             graph["T_PLUS"]=[g1,g2]
+    #             return (strexpr2Toks[1:], False, graph)
+    #         else:
+    #             return (tokens, True)
+    #     elif tokens[0] == "T_CONSTSTR":
+    #         return (tokens[1:], False, {"T_CONSTSTR": "a str"})
+    #     elif tokens[0] == "T_ID":
+    #         return (tokens[1:], False, {"T_ID": "an id"})
+    #     else:
+    #         return (tokens, True, graph)
+
     # 
     # PREDICATES
     #
-    def numbinopPred(self, token):
+    def binopPred(self, token):
         return token in ["T_PLUS", "T_MINUS", "T_MULT", "T_DIV", "T_MOD", "T_EXP"]
         
-    def numunopPred(self, token):
+    def unopPred(self, token):
         return token in ["T_MINUS", "T_SIN", "T_COS", "T_TAN"]
     
+    def constPred(self, token):
+        return token in ["T_CONSTSTR", "T_BOOL", "T_INT", "T_FLOAT"]
